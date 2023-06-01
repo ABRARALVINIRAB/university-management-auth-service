85506de1c261a682dd624331709406801b7ed176
abraralvinirab <abrar15-9800@diu.edu.bd>
2023-06-01 10:41:23 +0600

    setup eslint.prettier,husky lint staged

--- src/server.ts
+++ src/server.ts
@@ -1,19 +1,17 @@
-import mongoose from "mongoose";
-import app from "./app";
-import config from "./config/index";
+import mongoose from 'mongoose'
+import app from './app'
+import config from './config/index'
 
 async function main() {
-  try{
-    await mongoose.connect(config.database_url as string);
-    console.log('🛢 database connected succcesfully');
+  try {
+    await mongoose.connect(config.database_url as string)
+    console.log('🛢 database connected succcesfully')
     //listen
     app.listen(config.port, () => {
-  console.log(`application listening on port ${config.port}`)
-})
-  }catch(err){
-    console.log('failed to coonet in database',err);
+      console.log(`application listening on port ${config.port}`)
+    })
+  } catch (err) {
+    console.log('failed to coonet in database', err)
   }
-
-  
 }
-main();
+main()
